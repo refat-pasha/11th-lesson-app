0a2f2d2949a87036c28d6f829b1cfddce1ac09c1
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:32:07 +0600

    local storage support

--- eleventh_lesson_app/lib/moduls/offline/controllers/offline_controller.dart
+++ eleventh_lesson_app/lib/moduls/offline/controllers/offline_controller.dart
@@ -3,3 +3,9 @@ import 'package:get/get.dart';
 import '../../../data/models/material_model.dart';
 import '../../../data/providers/local_storage_provider.dart';
 import '../../../core/constants/storage_keys.dart';
+
+class OfflineController extends GetxController {
+    final LocalStorageProvider _storage = LocalStorageProvider();
+
+  final RxList<MaterialModel> offlineMaterials = <MaterialModel>[].obs;
+  final RxBool isLoading = false.obs;
