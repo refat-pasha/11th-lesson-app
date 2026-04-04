5ccbbdb8fb5be14e059f41c73bac5ded7e265d44
Safayet Hossain Nahin <safayetnahin11@gmail.com>
2026-04-04 21:07:28 +0600

    publication-binding

---
+++ eleventh_lesson_app/lib/moduls/publication/bindings/publication_binding.dart
@@ -0,0 +1,12 @@
+// placeholder
+// lib/modules/publication/bindings/publication_binding.dart
+
+import 'package:get/get.dart';
+import '../controllers/publication_controller.dart';
+
+class PublicationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<PublicationController>(() => PublicationController());
+  }
+}
