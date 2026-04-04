4ea84e9d35404f7bb6a3279e04ac49b4cf6b46ec
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:30:52 +0600

    routes added

---
+++ eleventh_lesson_app/lib/moduls/offline/controllers/offline_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+import '../../../data/models/material_model.dart';
+import '../../../data/providers/local_storage_provider.dart';
+import '../../../core/constants/storage_keys.dart';
