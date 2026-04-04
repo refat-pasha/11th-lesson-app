34185ef30e19cbcd4167adef0b6f6409b0188f64
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:14:18 +0600

    publication binding class added

--- eleventh_lesson_app/lib/moduls/progress/bindings/progress_binding.dart
+++ eleventh_lesson_app/lib/moduls/progress/bindings/progress_binding.dart
@@ -1,2 +1,9 @@
 import 'package:get/get.dart';
 import '../controllers/publication_controller.dart';
+
+class PublicationBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<PublicationController>(() => PublicationController());
+  }
+}
