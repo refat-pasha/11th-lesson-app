e4d88080aac9cb26e4b747f918bb66905c244c40
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:17:59 +0600

    fix on progress routes

--- eleventh_lesson_app/lib/moduls/progress/controllres/progress_controller.dart
+++ eleventh_lesson_app/lib/moduls/progress/controllres/progress_controller.dart
@@ -1,7 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:file_picker/file_picker.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_storage/firebase_storage.dart';
 
+import '../../../data/models/course_model.dart';
+import '../../../data/models/user_model.dart';
+import '../../../data/repositories/auth_repository.dart';
+import '../../../data/repositories/course_repository.dart';
 import '../../../data/providers/firebase_provider.dart';
