1f34b22157a70b68c97fb806ff54e364cf9cdd01
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:15:06 +0600

    controller needed routes added

---
+++ eleventh_lesson_app/lib/moduls/progress/controllres/progress_controller.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
+import '../../../data/providers/firebase_provider.dart';
