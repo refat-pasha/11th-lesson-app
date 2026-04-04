4d6744f6cf965ec01a4f5ded4616dc0b79c8246a
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:19:25 +0600

    firebase for progress controller

--- eleventh_lesson_app/lib/moduls/progress/controllres/progress_controller.dart
+++ eleventh_lesson_app/lib/moduls/progress/controllres/progress_controller.dart
@@ -5,3 +5,9 @@ import '../../../data/models/user_model.dart';
 import '../../../data/repositories/auth_repository.dart';
 import '../../../data/repositories/course_repository.dart';
 import '../../../data/providers/firebase_provider.dart';
+
+
+class ProgressController extends GetxController {
+  final AuthRepository authRepository = Get.find();
+  final CourseRepository courseRepository = Get.find();
+  final FirebaseProvider firebaseProvider = Get.find();
