4fd80324c48feb79b8d098aa22ed55f73c254f23
turtle <141565698+refat-pasha@users.noreply.github.com>
2026-04-04 23:35:25 +0600

    offline view UI

--- eleventh_lesson_app/lib/moduls/offline/views/offline_view.dart
+++ eleventh_lesson_app/lib/moduls/offline/views/offline_view.dart
@@ -3,3 +3,29 @@ import 'package:get/get.dart';
 
 import '../controllers/offline_controller.dart';
 import '../../../app/theme/colors.dart';
+
+class OfflineView extends GetView<OfflineController> {
+  const OfflineView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.backgroundDark,
+      appBar: AppBar(
+        title: const Text("Offline Materials"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Obx(() {
+        if (controller.isLoading.value) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        if (controller.offlineMaterials.isEmpty) {
+          return const Center(
+            child: Text(
+              "No offline materials",
+              style: TextStyle(color: Colors.white70),
+            ),
+          );
+        }
