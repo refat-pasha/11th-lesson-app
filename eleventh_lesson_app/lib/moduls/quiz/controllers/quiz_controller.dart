163c6bfcb3dbe81bb56f7c0d17ecdcf41c196647
sumaiyamoon16 <sumaiyamoon16@gmail.com>
2026-04-10 15:51:53 +0600

    quizzzzz

--- eleventh_lesson_app/lib/moduls/quiz/controllers/quiz_controller.dart
+++ eleventh_lesson_app/lib/moduls/quiz/controllers/quiz_controller.dart
@@ -1,4 +1,62 @@
 import 'package:get/get.dart';
+import '../../../core/controllers/lms_controller.dart';
+class DashboardController extends GetxController {
+  final lms = Get.find<LmsController>();
+  Future<void> refreshDashboard() async {
+    update();
+  }
+}
+
+import 'package:get/get.dart';
+
+import '../../../core/controllers/lms_controller.dart';
+
+class DashboardController extends GetxController {
+  final lms = Get.find<LmsController>();
+
+  Future<void> refreshDashboard() async {
+    update();
+  }
+}
+
+import 'package:get/get.dart';
+
+import '../../../core/controllers/lms_controller.dart';
+
+class DashboardController extends GetxController {
+  final lms = Get.find<LmsController>();
+
+  Future<void> refreshDashboard() async {
+    update();
+  }
+}
+import 'package:get/get.dart';
+
+import '../../../core/controllers/lms_controller.dart';
+
+import 'package:get/get.dart';
+
+import '../../../core/controllers/lms_controller.dart';
+
+class DashboardController extends GetxController {
+  final lms = Get.find<LmsController>();
+
+  Future<void> refreshDashboard() async {
+    update();
+  }
+}
+import 'package:get/get.dart';
+
+import '../../../core/controllers/lms_controller.dart';
+
+class DashboardController extends GetxController {
+  final lms = Get.find<LmsController>();
+
+  Future<void> refreshDashboard() async {
+    update();
+  }
+}
+import 'package:get/get.dart';
 
 import '../../../core/controllers/lms_controller.dart';
 
